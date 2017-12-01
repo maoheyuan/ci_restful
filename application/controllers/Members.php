321f336989ee70bd2ea01444e8d02a95679910e7
“maoheyuan <maoheyuan@qq.com>
2017-12-01 09:34:09 +0800

    修改会员模块开发

--- application/controllers/Members.php
+++ application/controllers/Members.php
@@ -20,7 +20,7 @@ class Members extends MY_Controller {
             else {
                 $this->response([
                     'status' => FALSE,
-                    'message' => 'No users were found'
+                    'message' => '会员列表不存在!'
                 ], MY_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404)
             }
         }
@@ -37,73 +37,74 @@ class Members extends MY_Controller {
                 else {
                     $this->response([
                         'status' => FALSE,
-                        'message' => 'User could not be found'
+                        'message' => '会员不存在'
                     ], MY_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404)
                 }
             }
         }
     }
+
     public  function  validation($post=array(),$type="put"){
         if(!$post["username"]){
             $this->response([
                 'status' => FALSE,
                 'message' => '用户名不能为空'
-            ], MY_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404)
+            ], MY_Controller::HTTP_BAD_REQUEST); // NOT_FOUND (404)
         }
         if(!$post["realname"]){
             $this->response([
                 'status' => FALSE,
                 'message' => '真实姓名不能为空'
-            ], MY_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404)
+            ], MY_Controller::HTTP_BAD_REQUEST); // NOT_FOUND (404)
         }
         if(!$post["password"]){
             $this->response([
                 'status' => FALSE,
                 'message' => '密码不能为空'
-            ], MY_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404)
+            ], MY_Controller::HTTP_BAD_REQUEST); // NOT_FOUND (404)
         }
         if(!$post["rpassword"]){
             $this->response([
                 'status' => FALSE,
                 'message' => '确认密码不能为空'
-            ], MY_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404)
+            ], MY_Controller::HTTP_BAD_REQUEST); // NOT_FOUND (404)
         }
         if($post["password"]!=$post["rpassword"]){
             $this->response([
                 'status' => FALSE,
                 'message' => '二次输入的密码不一致'
-            ], MY_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404)
+            ], MY_Controller::HTTP_BAD_REQUEST); // NOT_FOUND (404)
         }
 
         if(!$post["mobile"]){
             $this->response([
                 'status' => FALSE,
                 'message' => '手机号不能为空'
-            ], MY_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404)
+            ], MY_Controller::HTTP_BAD_REQUEST); // NOT_FOUND (404)
         }
         if(!$post["status"]){
             $this->response([
                 'status' => FALSE,
                 'message' => '状态不能为空'
-            ], MY_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404)
+            ], MY_Controller::HTTP_BAD_REQUEST); // NOT_FOUND (404)
         }
         if(!$post["account"]){
             $this->response([
                 'status' => FALSE,
                 'message' => '账号不能为空'
-            ], MY_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404)
+            ], MY_Controller::HTTP_BAD_REQUEST); // NOT_FOUND (404)
         }
         if(!$post["address"]){
             $this->response([
                 'status' => FALSE,
                 'message' => '地址不能为空'
-            ], MY_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404)
+            ], MY_Controller::HTTP_BAD_REQUEST); // NOT_FOUND (404)
         }
         if($type=="post"&&!$post["id"]){
             $this->response([
                 'status' => FALSE,
                 'message' => 'id不能为空'
-            ], MY_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404)
+            ], MY_Controller::HTTP_BAD_REQUEST); // NOT_FOUND (404)
         }
     }
 
@@ -114,27 +115,30 @@ class Members extends MY_Controller {
         if(!$post["id"]){
             $this->response([
                 'status' => FALSE,
-                'message' => 'User could not be found'
-            ], MY_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404)
+                'message' => '新增失败'
+            ], MY_Controller::HTTP_BAD_REQUEST); // NOT_FOUND (404)
         }
         else{
+
             $this->response($post, MY_Controller::HTTP_OK); // OK (200)
         }
     }
+
     public  function post(){
         $post=$this->input->post();
         $this->validation($post);
 
-        if($this->member_model->update($post["id"],$post)){
+        if(!$this->member_model->update($post["id"],$post)){
             $this->response([
                 'status' => FALSE,
-                'message' => 'User could not be found'
-            ], MY_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404)
+                'message' => '修改失败'
+            ], MY_Controller::HTTP_BAD_REQUEST); // NOT_FOUND (404)
         }
         else{
             $this->response($post, MY_Controller::HTTP_OK); // OK (200)
         }
     }
+
 
     public  function delete($id=NULL){
         if($id==NULL){
@@ -144,17 +148,17 @@ class Members extends MY_Controller {
         if ($id <= 0) {
             $this->response([
                 'status' => FALSE,
-                'message' => 'id不能为空'
-            ], MY_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404)
+                'message' => '参数不正确'
+            ], MY_Controller::HTTP_BAD_REQUEST); // NOT_FOUND (400)
         }
-        if($this->member_model->delete($id)){
+        if(!$this->member_model->delete($id)){
             $this->response([
                 'status' => FALSE,
-                'message' => 'User could not be found'
-            ], MY_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404)
+                'message' => '删除失败'
+            ],  MY_Controller::HTTP_BAD_REQUEST); // NOT_FOUND (404)
         }
         else{
-            $this->response(NULL, MY_Controller::HTTP_OK); // OK (200)
+            $this->response(NULL, MY_Controller::HTTP_NO_CONTENT); // OK (200)
         }
     }
 
